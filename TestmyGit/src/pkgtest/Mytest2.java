9d73218261695d3df9b27162dc7d35a2d7bcf7bf
rama_70 <rama_70@yahoo.com>
2018-09-17 08:42:47 +0530

    test push

--- TestmyGit/src/pkgtest/Mytest2.java
+++ TestmyGit/src/pkgtest/Mytest2.java
@@ -2,12 +2,7 @@ package pkgtest;
 
 public class Mytest2 {
 
-	public void myfirst(){
-		// this first method
-	}
-	public static void main(String ara[]){
-		System.out.println("this is first line");
-	}
+	
 
 
 }
